011dcfe3c223b2647c59ecaf3dd37062a99a2e57
abrandhawa <alibakhat01@gmail.com>
2024-01-24 13:20:05 +0500

    added routes to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    },{
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'search',
+        component:SearchComponent
+    },
+    {
+        path:'bookings',
+        component:BookingsComponent
+    },{
+        path:'admin',
+        component:AdminComponent
+        
+    }
+];
